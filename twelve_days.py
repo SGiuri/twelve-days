47bd37618587e987dd20932617aa4e26fb92eed8
SGiuri <s.giuri@issm.it>
2020-10-02 13:31:47 +0200

    Done

--- twelve_days.py
+++ twelve_days.py
@@ -1,6 +1,7 @@
 import re
-def recite(start_verse, end_verse):
 
+
+def recite(start_verse, end_verse):
     verse_12 = """
     On the twelfth day of Christmas my true love gave to me: 
     twelve Drummers Drumming, 
@@ -20,51 +21,38 @@ def recite(start_verse, end_verse):
     days = ["first",
             "second",
             "third",
-            "forth",
+            "fourth",
             "fifth",
             "sixth",
             "seventh",
-            "eigth",
-            "nineth",
+            "eighth",
+            "ninth",
             "tenth",
             "eleventh",
             "twelfth"]
 
+    the_song = []
+
+    # splitting the twelfth verse
     verse_parts = [verse.lstrip() for verse in verse_12.strip().split("\n")]
+    intro_verse = verse_parts.pop(0)
 
-    intro = verse_parts.pop[0]
-    requested_song = []
-    # creating first verse
+    # building each verse
+    for day in range(start_verse, end_verse + 1):
 
-    for day in range(start_verse,end_verse+1):
-        day_intro = re.sub(r"twelfth", days[day - 1], intro)
-        the_verse = day_intro
+        days_intro = re.sub(r"twelfth", days[day - 1], intro_verse)
+
         if day == 1:
-            the_verse += verse_parts[11][4:]
+            requested_verse = days_intro + list_to_str(verse_parts[11][4:])
         else:
+            requested_verse = days_intro + list_to_str(verse_parts[12 - day:])
+        the_song.append(requested_verse)
 
-            the_verse += verse_parts[13-day:]
-        if start_verse == end_verse:
-            return the_verse
-        requested_song.append(the_verse)
-
-    return requested_song
+    return the_song
 
 
-expected = [
-    "On the twelfth day of Christmas my true love gave to me: "
-    "twelve Drummers Drumming, "
-    "eleven Pipers Piping, "
-    "ten Lords-a-Leaping, "
-    "nine Ladies Dancing, "
-    "eight Maids-a-Milking, "
-    "seven Swans-a-Swimming, "
-    "six Geese-a-Laying, "
-    "five Gold Rings, "
-    "four Calling Birds, "
-    "three French Hens, "
-    "two Turtle Doves, "
-    "and a Partridge in a Pear Tree."
-]
-
-print(recite(12,12))
+def list_to_str(list_of_string):
+    final_stirng = ""
+    for j in range(len(list_of_string)):
+        final_stirng += list_of_string[j]
+    return final_stirng

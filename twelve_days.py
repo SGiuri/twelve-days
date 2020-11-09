4aa4f523fa9bb49c18c58414e53285ca472854ef
SGiuri <s.giuri@issm.it>
2020-11-09 10:28:40 +0100

    joining instead of iterating

--- twelve_days.py
+++ twelve_days.py
@@ -1,17 +1,17 @@
 def recite(start_verse, end_verse):
 
     presents = ['a Partridge in a Pear Tree.',
-                'two Turtle Doves, ',
-                'three French Hens, ',
-                'four Calling Birds, ',
-                'five Gold Rings, ',
-                'six Geese-a-Laying, ',
-                'seven Swans-a-Swimming, ',
-                'eight Maids-a-Milking, ',
-                'nine Ladies Dancing, ',
-                'ten Lords-a-Leaping, ',
-                'eleven Pipers Piping, ',
-                'twelve Drummers Drumming, ']
+                'two Turtle Doves',
+                'three French Hens',
+                'four Calling Birds',
+                'five Gold Rings',
+                'six Geese-a-Laying',
+                'seven Swans-a-Swimming',
+                'eight Maids-a-Milking',
+                'nine Ladies Dancing',
+                'ten Lords-a-Leaping',
+                'eleven Pipers Piping',
+                'twelve Drummers Drumming']
 
     days = ["first",
             "second",
